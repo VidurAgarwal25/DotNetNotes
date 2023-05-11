9419afc0c365d1721adde5a623c26646d8da993e
Vidur Agarwal <geurecruitviduragarwal@gmail.com>
2023-05-11 19:17:29 +0530

    TemplateHtmlHelper

---
+++ TemplatedHtmlHelper/TemplatedHtmlHelper/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TemplatedHtmlHelper.Models;
+
+namespace TemplatedHtmlHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Employee employee = new Employee()
+            {
+                Id = 1,
+                Name = "Vidur",
+                Email = "vidur025agarwal@gmail.com",
+                DateOfBirth = DateTime.Now,
+                IsOnline = true
+            };
+            return View(employee);
+        }
+    }
+}
---
+++ TemplatedHtmlHelper/TemplatedHtmlHelper/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace TemplatedHtmlHelper.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public String Email { get; set; }
+        public bool IsOnline { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}

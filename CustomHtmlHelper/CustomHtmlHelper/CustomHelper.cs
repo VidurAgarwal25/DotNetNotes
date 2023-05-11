8843b51508f204c68db87dc619827924b1742531
Vidur Agarwal <geurecruitviduragarwal@gmail.com>
2023-05-11 23:16:30 +0530

    customhelper

---
+++ CustomHtmlHelper/CustomHtmlHelper/CustomHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc;
+namespace CustomHtmlHelper
+{
+    public static class CustomHelper
+    {
+        public static IHtmlString Image(string src, string alt)
+        {
+            return new MvcHtmlString(string.Format("<image src='{0}' alt='{1}'></img>",src, alt));
+        }
+    }
+}
